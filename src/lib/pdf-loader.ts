9822f6fb3d24aee631c1c70e3c22f74f56cf0d00
VikhilP <vikhilp98@gmail.com>
2024-02-13 13:44:57 +0000

    Initial change

--- src/lib/pdf-loader.ts
+++ src/lib/pdf-loader.ts
@@ -1,21 +1,57 @@
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { env } from "./config";
+import fs from "fs"
+
+import { promisify } from 'util';
+
+// Promisify fs.readFile
+const readFileAsync = promisify(fs.readFile);
 
 export async function getChunkedDocsFromPDF() {
   try {
+    const filePath = "./docs/sections.json";
+    const filePathTemp = "./docs/temp.json";
+
     const loader = new PDFLoader(env.PDF_PATH);
     const docs = await loader.load();
 
     // From the docs https://www.pinecone.io/learn/chunking-strategies/
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+
+    //this is normally returned but i needed to send sections itself
+    /* const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
     });
 
-    const chunkedDocs = await textSplitter.splitDocuments(docs);
+    const chunkedDocs = await textSplitter.splitDocuments(docs); */
+    let sections: any[] = [];
 
-    return chunkedDocs;
+    // Read the file asynchronously using Promisify
+    const data = await readFileAsync(filePath);
+    const gram = JSON.parse(data.toString());
+    const keys = Object.keys(gram);
+
+    keys.forEach((k) => {
+        sections.push({ "pageContent": JSON.stringify(gram[k]) });
+    });
+
+    console.log(sections);
+    return sections;
+
+    
+    //   fs.writeFile(filePathTemp, JSON.stringify(chunkedDocs), { encoding: "utf-8" }, (err: NodeJS.ErrnoException | null) => {
+    //   if (err) {
+    //     console.error("Error writing file:", err);
+    //   } else {
+    //     console.log("File written successfully.");
+    //   }
+    // });
+      //console.log(sections)
+
+    //return []
+
+    
   } catch (e) {
     console.error(e);
     throw new Error("PDF docs chunking failed !");
